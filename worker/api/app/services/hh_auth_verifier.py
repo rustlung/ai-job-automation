e43c30cae3c4d085f864c129c8b52e78f958a400
Rustlung <v.i.sigaev@gmail.com>
2026-08-19 13:13:26 +0300

    Revert "fix(worker): update HH authentication verificat This reverts commit e56e3d41c9006f57fe44157cc752cce07c76226a.

--- worker/api/app/services/hh_auth_verifier.py
+++ worker/api/app/services/hh_auth_verifier.py
@@ -10,9 +10,8 @@ LOGIN_SELECTORS = (
     'form[action*="account"]',
     'input[type="tel"]',
     'input[name="login"]',
-    '[data-qa="login"]',
-    '[data-qa="login-form"]',
-    '[data-qa="account-login"]',
+    '[data-qa*="login"]',
+    '[data-qa*="account-login"]',
 )
 AUTHENTICATED_MARKER_SELECTORS = (
     '[data-qa="mainmenu_applicantProfile"]',
@@ -44,14 +43,7 @@ class HHAuthenticationVerificationResult:
 
     @property
     def authenticated(self) -> bool:
-        return (
-            self.storage_state_loaded
-            and not self.login_form_detected
-            and (
-                self.authenticated_marker_detected
-                or (self.resume_context_marker_detected and self.vacancy_count > 0)
-            )
-        )
+        return self.storage_state_loaded and self.authenticated_marker_detected and not self.login_form_detected
 
     @property
     def resume_context_confirmed(self) -> bool:
